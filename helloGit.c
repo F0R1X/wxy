4822de8f8356fd2cbc9043053c11d46af83a412e
F0R1X <xuqing198912@163.com>
2016-06-03 05:38:26 -0700

    firstcode

---
+++ helloGit.c
@@ -0,0 +1,16 @@
+                                     /**********
+************************************************
+**     Say   Hello to Git! I'm coming...      **
+**                  time:2016/06/03/ 20:28    **
+************************************************
+*******/
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char *chShow = "Hello, Git!";
+	printf("%s\n", chShow);
+	
+	//string str = "Hello, Git!";
+	//printf("%s\n", str);
+}
